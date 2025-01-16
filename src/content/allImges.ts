c8c952d127f1d29e0a27cf37dd9491e8990cd2d0
Namrata Chandarana <namratachandarana20@gmail.com>
2025-01-16 17:16:55 +0530

    update features section

---
+++ src/content/allImges.ts
@@ -0,0 +1,13 @@
+interface images {
+  src: string,
+  alt: string
+}
+
+export const TurstedByImages: images[] = [
+  { src: "../assets/features/image1.png", alt: "BETA" },
+  { src: "../assets/features/image2.png", alt: "incus" },
+  { src: "../assets/features/image3.png", alt: "UNIVERSE" },
+  { src: "../assets/features/image4.png", alt: "BLUE SPEC" },
+  { src: "../assets/features/image5.png", alt: "UNCLE CHARLY" },
+  { src: "../assets/features/image6.png", alt: "THE CONTAINER" },
+];
