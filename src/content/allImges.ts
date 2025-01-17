1616554c9ae4ee30f5f7747a0df9c1976a744319
Namrata Chandarana <namratachandarana20@gmail.com>
2025-01-17 16:33:27 +0530

    all images set

--- src/content/allImges.ts
+++ src/content/allImges.ts
@@ -1,23 +1,31 @@
-interface images {
-  src: string,
-  alt: string
-}
+import trustedImg1 from '../asset/features/image1.png'
+import trustedImg2 from '../asset/features/image2.png'
+import trustedImg3 from '../asset/features/image3.png'
+import trustedImg4 from '../asset/features/image4.png'
+import trustedImg5 from '../asset/features/image5.png'
+import trustedImg6 from '../asset/features/image6.png'
 
-export const TurstedByImages: images[] = [
-  { src: "../assets/features/image1.png", alt: "BETA" },
-  { src: "../assets/features/image2.png", alt: "incus" },
-  { src: "../assets/features/image3.png", alt: "UNIVERSE" },
-  { src: "../assets/features/image4.png", alt: "BLUE SPEC" },
-  { src: "../assets/features/image5.png", alt: "UNCLE CHARLY" },
-  { src: "../assets/features/image6.png", alt: "THE CONTAINER" },
+import featuresImg1 from '../asset/features/Frame1.png'
+import featuresImg2 from '../asset/features/Frame2.png'
+import featuresImg3 from '../asset/features/Frame3.png'
+import featuresImg4 from '../asset/features/Frame4.png'
+import featuresImg5 from '../asset/features/Frame5.png'
+import featuresImg6 from '../asset/features/Frame6.png'
+
+export const TurstedByImages= [
+  { src: trustedImg1, alt: "BETA" },
+  { src: trustedImg2, alt: "incus" },
+  { src: trustedImg3, alt: "UNIVERSE" },
+  { src: trustedImg4, alt: "BLUE SPEC" },
+  { src: trustedImg5, alt: "UNCLE CHARLY" },
+  { src: trustedImg6, alt: "THE CONTAINER" },
 ];
 
-
-export const workspaceImages: images[] = [
-  { src: "../assets/features/Frame1.png", alt: "" },
-  { src: "../assets/features/Frame2.png", alt: "" },
-  { src: "../assets/features/Frame3.png", alt: "" },
-  { src: "../assets/features/Frame4.png", alt: "" },
-  { src: "../assets/features/Frame5.png", alt: "" },
-  { src: "../assets/features/Frame6.png", alt: "" },
+export const workspaceImages = [
+  { src: featuresImg1.src, alt: "" },
+  { src: featuresImg2.src, alt: "" },
+  { src: featuresImg3.src, alt: "" },
+  { src: featuresImg4.src, alt: "" },
+  { src: featuresImg5.src, alt: "" },
+  { src: featuresImg6.src, alt: "" },
 ]
